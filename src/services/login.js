905a9b80904c3d5f0dd0ce9a0f10ba4e54989ad4
Ekamjot <ekamjot.chugh1004@gmail.com>
2023-04-07 15:26:28 +0530

    backend

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/login'
+const baseUrl = 'https://ekams-notes.onrender.com/api/login'
 
 const login = async credentials => {
   const response = await axios.post(baseUrl, credentials)
--- src/services/users.js
+++ src/services/users.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/users'
+const baseUrl = 'https://ekams-notes.onrender.com/api/users'
 
 const getAll = () => {
   
